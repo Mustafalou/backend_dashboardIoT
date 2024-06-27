7dbfe98b0523e863ed8005da39e9b2510ca62fb5
Mustafalou <yilmaz121030@hotmail.be>
2024-06-27 16:26:54 +0200

    allow everyone

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 const corsOptions = {
-  origin:"http://localhost:3000",
+  origin:"*",
   credentials:true,
 } 
 app.use(cors(corsOptions));
