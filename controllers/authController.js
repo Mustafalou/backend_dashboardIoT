6d0d535e636658d32b8235e45c4b0a551c729a5d
Mustafalou <yilmaz121030@hotmail.be>
2024-07-15 16:25:06 +0200

    refactor: Update login route to set longer expiration time for access token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,6 @@
-// controllers/authController.js
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { User } = require('../models/db'); // Assuming you have a User model in models directory
+const { User } = require('../models/db');
 const userActivityController = require('./userActivityController');
 
 const secretKey = 'your_secret_key';
@@ -15,9 +14,8 @@ exports.login = async (req, res) => {
     if (user) {
       const match = await bcrypt.compare(password, user.password);
       if (match) {
-        // Generate JWT
         const token = jwt.sign({ userId: user.id, userEmail:user.email }, secretKey, { expiresIn: '1h' });
-        res.cookie('accessToken', token, { httpOnly: true, secure: false, sameSite: 'strict', maxAge: 3600000 });
+        res.cookie('accessToken', token, { httpOnly: true, secure: false, sameSite: 'strict', maxAge: 1000*60*60*12 });
         await userActivityController.logActivity(user.id,user.email, 'login', `User logged in`);
         res.send({ message: "Logged in successfully" });
       
